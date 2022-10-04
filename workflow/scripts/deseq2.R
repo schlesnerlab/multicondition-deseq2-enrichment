0eed0da5b858b22e0549cabe26a78ce88568cebd
Christian Heyer <c.heyer@dkfz-heidelberg.de>
2022-10-04 13:17:35 +0200

    Fixed old bug

--- workflow/scripts/deseq2.R
+++ workflow/scripts/deseq2.R
@@ -15,26 +15,10 @@ if (snakemake@threads > 1) {
 dds <- readRDS(snakemake@input[[1]])
 
 contrast <- c(snakemake@wildcards[["condition"]], snakemake@params[["contrast"]])
-print(contrast)
-if (snakemake@config[["perform_perturbation"]]) {
-  # Res
-  index <- (dds@colData$condition %in% snakemake@params[["contrast"]])
-  cond_labels <- dds@colData$condition[index]
-  pert_cond_labels <- cond_labels
-  while (isTRUE(all.equal(pert_cond_labels, cond_labels))) {
-    pert_cond_labels <- sample(cond_labels,
-      length(cond_labels),
-      replace = F
-    )
-  }
-  print(pert_cond_labels)
-  dds@colData[index, "condition"] <- pert_cond_labels
-}
-#dds$condition <- relevel(dds$condition, contrast[3])
 res <- results(dds, contrast = contrast, parallel = parallel)
 resstat <- res$stat
 # shrink fold changes for lowly expressed genes
-res <- lfcShrink(dds, contrast = contrast, res = res,type = "ashr")
+res <- lfcShrink(dds, contrast = contrast, res = res, type = "ashr")
 # sort by p-value
 res$stat <- resstat
 res <- res[,c(
